1c8e4626c47013b4dcae3708341bcf38114fa6c6
Leonid <x.1.x2@yandex.ru>
2024-06-23 19:34:11 +0400

    Save before merge the first branch

--- Dispatcher/src/main/java/org/example/Controllers/TelegramBot.java
+++ Dispatcher/src/main/java/org/example/Controllers/TelegramBot.java
@@ -3,7 +3,9 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Log4j
 @Component
@@ -29,6 +31,21 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         var Message = update.getMessage();
         log.debug(Message.getText());
+
+        var response = new SendMessage();
+        response.setChatId(Message.getChatId().toString());
+        response.setText("__Приветствие бота__");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 
 
