774c79405e17a14b68a07c7c5514843e53097746
Leonid <x.1.x2@yandex.ru>
2024-07-02 16:47:48 +0400

    Queue Rabbit is Work

--- Common-RabitMQ/src/main/java/org/example/Models/RabbitQueue.java
+++ Common-RabitMQ/src/main/java/org/example/Models/RabbitQueue.java
@@ -4,5 +4,5 @@ public class RabbitQueue {
     public static final String DOC_MESSAGE_UPDATE = "doc_message_update";
     public static final String PHOTO_MESSAGE_UPDATE = "photo_message_update";
     public static final String TEXT_MESSAGE_UPDATE = "text_message_update";
-    public static final String ANSWER_MESSAGE_UPDATE = "answer_message";
+    public static final String ANSWER_MESSAGE = "answer_message";
 }
--- Dispatcher/src/main/java/org/example/Services/Impl/UpdateProducerImpl.java
+++ Dispatcher/src/main/java/org/example/Services/Impl/UpdateProducerImpl.java
@@ -2,14 +2,22 @@ package org.example.Services.Impl;
 
 import lombok.extern.log4j.Log4j;
 import org.example.Services.UpdateProducer;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
-    public void producer(String rabitQueue, Update update) {
+    public void producer(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
