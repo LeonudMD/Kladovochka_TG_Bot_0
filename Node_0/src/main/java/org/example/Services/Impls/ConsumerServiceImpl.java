d12e2bd8254d6322eef1a8f2598e97e737ed0c9a
Leonid <x.1.x2@yandex.ru>
2024-07-02 19:19:35 +0400

    Impl Node Services & RabitConfig for Node

--- Node_0/src/main/java/org/example/Services/Impls/ConsumerServiceImpl.java
+++ Node_0/src/main/java/org/example/Services/Impls/ConsumerServiceImpl.java
@@ -1,4 +1,46 @@
 package org.example.Services.Impls;
 
-public class ConsumerServiceImpl {
+import lombok.extern.log4j.Log4j;
+import org.example.Services.ConsumerService;
+import org.example.Services.ProducerService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static org.example.Models.RabbitQueue.*;
+
+@Service
+@Log4j
+public class ConsumerServiceImpl implements ConsumerService {
+    private final ProducerService producerService;
+
+    public ConsumerServiceImpl(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+
+    @Override
+    @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
+    public void consumeTextMessageUpdate(Update update) {
+        log.debug("NODE: Текстовое сообщение было доставленно");
+
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("hi, FROM NODE");
+        producerService.produceAnswer(sendMessage);
+    }
+
+    @RabbitListener(queues = DOC_MESSAGE_UPDATE)
+    @Override
+    public void consumeDocMessageUpdate(Update update) {
+        log.debug("NODE: Док сообщение было доставленно");
+    }
+
+    @RabbitListener(queues = PHOTO_MESSAGE_UPDATE)
+    @Override
+    public void consumePhotoMessageUpdate(Update update) {
+        log.debug("NODE: Фото сообщение было доставленно");
+    }
 }
--- Node_0/src/main/java/org/example/Services/Impls/ProducerServiceImpl.java
+++ Node_0/src/main/java/org/example/Services/Impls/ProducerServiceImpl.java
@@ -1,4 +1,22 @@
 package org.example.Services.Impls;
 
-public class ProducerServiceImpl {
+import org.example.Services.ProducerService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static org.example.Models.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class ProducerServiceImpl implements ProducerService {
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProducerServiceImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void produceAnswer(SendMessage sendMessage) {
+        rabbitTemplate.convertAndSend(ANSWER_MESSAGE, sendMessage);
+    }
 }
