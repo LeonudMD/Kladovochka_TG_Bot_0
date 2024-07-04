5598b13f848bfa03312a5124c9c44d4bc34064f4
Leonid <x.1.x2@yandex.ru>
2024-07-04 15:32:17 +0400

    Added RawData from Entity

--- Node_0/src/main/java/org/example/Entity/RawData.java
+++ Node_0/src/main/java/org/example/Entity/RawData.java
@@ -1,4 +1,30 @@
 package org.example.Entity;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Raw_Data")
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 public class RawData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "JsonB")
+    private Update event;
+
 }
