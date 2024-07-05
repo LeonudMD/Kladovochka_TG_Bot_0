023260d167fd2bd03b83df1815c2ffc1da4bf481
Leonid <x.1.x2@yandex.ru>
2024-07-05 16:33:32 +0400

    BinaryContent ENTITY && DAO

--- Common-JPA/src/main/java/org/example/DAO/AppDocumentDAO.java
+++ Common-JPA/src/main/java/org/example/DAO/AppDocumentDAO.java
@@ -2,7 +2,6 @@ package org.example.DAO;
 
 import org.example.Entity.AppDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.relex.entity.AppDocument;
 
 public interface AppDocumentDAO extends JpaRepository<AppDocument, Long> {
 }
