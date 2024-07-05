93646586f7dd138ef920ab03d637d9557df48836
Leonid <x.1.x2@yandex.ru>
2024-07-05 16:29:03 +0400

    Document App_ENTITY && DAO

---
+++ Common-JPA/src/main/java/org/example/Entity/AppDocument.java
@@ -0,0 +1,26 @@
+package org.example.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_document")
+public class AppDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String telegramFileId;
+    private String docName;
+    @OneToOne
+    private BinaryContent  binaryContent;
+    private String mimeType;
+    private Long fileSize;
+}
