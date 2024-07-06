84962a6eb86c357658b9f1ada0a627a68db1e95b
Leonid <x.1.x2@yandex.ru>
2024-07-06 20:07:46 +0400

    Added RestServicesConfiguration

---
+++ Rest-Service/src/main/java/org/example/Configurations/RestServicesConfiguration.java
@@ -0,0 +1,17 @@
+package org.example.Configurations;
+
+import org.example.Utils.CryptoTool;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestServicesConfiguration {
+    @Value("${salt}")
+    private String salt;
+
+    @Bean
+    public CryptoTool getCryptoTool() {
+        return new CryptoTool(salt);
+    }
+}
