8427bf21899fc8802ddfa6e05fe4db6bdc45075c
Leonid <x.1.x2@yandex.ru>
2024-07-10 13:14:57 +0400

    Added MAilParams_DTO

---
+++ Mail-Service/src/main/java/org/example/DTO/MailParams.java
@@ -0,0 +1,13 @@
+package org.example.DTO;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MailParams {
+    private String id;
+    private String emailTo;
+}
