0527045fb2204c5fbc0f11d29ba15c15fbf2cc57
Leonid <x.1.x2@yandex.ru>
2024-07-10 14:41:09 +0400

    AddedMailController

---
+++ Mail-Service/src/main/java/org/example/Controllers/MailController.java
@@ -0,0 +1,25 @@
+package org.example.Controllers;
+
+import org.example.DTO.MailParams;
+import org.example.Services.MailSenderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/mail")
+@RestController
+public class MailController {
+    private final MailSenderService mailSenderService;
+
+    public MailController(MailSenderService mailSenderService) {
+        this.mailSenderService = mailSenderService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<?> sendActivationMail(@RequestBody MailParams mailParams) {
+        mailSenderService.send(mailParams);
+        return ResponseEntity.ok().build();
+    }
+}
